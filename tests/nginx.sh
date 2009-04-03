3e3f46787d66f6a31ecb9ebc1465a5ebda2fa969
Joe Khoobyar <joe@collectivex.com>
2009-04-03 17:28:20 -0400

    fixed paths in test script

--- tests/nginx.sh
+++ tests/nginx.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ocf-tester -n nginx-test -o nginx=$(which nginx) -o config=$(pwd)/config/nginx.conf $(dirname $0)../joekhoobyar/NGINX
+ocf-tester -n nginx-test -o nginx=$(which nginx) -o config=$(dirname $0)/config/nginx.conf $(dirname $0)/../joekhoobyar/NGINX

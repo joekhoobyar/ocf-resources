1d2824717dcf8a304c3dfd031bd19758e1d2d486
Joe Khoobyar <joe@collectivex.com>
2009-04-04 00:37:44 -0400

    make the tests less likely to overwrite anything important

--- tests/nginx.sh
+++ tests/nginx.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+mkdir -p /tmp/nginx-html &&
+	touch /tmp/nginx-html/index.html
+
 ocf-tester -n nginx-test \
 	-o nginx=$(which nginx) \
 	-o config=$(dirname $0)/config/nginx.conf \
 	-o pid=/tmp/nginx.pid \
 	-o log=/tmp/nginx.log \
+	-o monitor_url=http://localhost:8088/ \
 	$(dirname $0)/../joekhoobyar/NGINX
 RETVAL=$?
 

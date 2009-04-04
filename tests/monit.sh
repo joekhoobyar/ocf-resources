45449e7cc9e12e6cd91ee9088a6057ef3d80fd38
Joe Khoobyar <joe@collectivex.com>
2009-04-04 00:29:59 -0400

    make the tests less likely to overwrite anything important

--- tests/monit.sh
+++ tests/monit.sh
@@ -6,11 +6,12 @@ ocf-tester -n monit-test \
 	-o monit=$(which monit) \
 	-o config=$(dirname $0)/config/monitrc \
 	-o log=/tmp/monit.log \
+	-o pid=/tmp/monit.pid \
 	-o state=/tmp/monit.state \
 	$(dirname $0)/../joekhoobyar/monit
 RETVAL=$?
 
-rm -f /tmp/monit.{log,state}
+rm -f /tmp/monit.{log,pid,state}
 chmod 644 $(dirname $0)/config/monitrc
 
 exit $RETVAL

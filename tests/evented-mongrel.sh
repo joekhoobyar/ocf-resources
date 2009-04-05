65fae052fdb41bda46afa68f41ae2af4220bce5f
Joe Khoobyar <joe@collectivex.com>
2009-04-04 20:47:15 -0400

    try testing evented mongrels too

---
+++ tests/evented-mongrel.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#
+# Mongrel OCF resource agent tests.
+#
+# Author:		Joe Khoobyar <joe@ankhcraft.com>
+# License:	GNU General Public License (GPL) version 2
+# Copyright (c) 2009 All Rights Reserved
+#
+
+rm -rf /tmp/mongrel-test
+echo "Generating a test Rails app in /tmp/mongrel-test..."
+rails /tmp/mongrel-test -d sqlite3 2>/dev/null >/dev/null
+cp $(dirname $0)/config/mongrel.yml /tmp/mongrel-test/config/mongrel.yml
+
+ocf-tester -n mongrel-test \
+	-o mongrel="env EVENT=1 $(which mongrel_rails)" \
+	-o config=/tmp/mongrel-test/config/mongrel.yml \
+	-o monitor_url=http://localhost:8088/ \
+	$(dirname $0)/../joekhoobyar/Mongrel
+RETVAL=$?
+
+rm -f /tmp/mongrel.{log,pid}
+rm -rf /tmp/mongrel-test
+
+exit $RETVAL
